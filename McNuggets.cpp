ced9e10e1a95a6cd7226731b4fdba3f2dc0e75a2
Mohidul Haque Mridul <mridul.haque.mh@gmail.com>
2021-08-10 23:59:11 +0600

    math

---
+++ McNuggets.cpp
@@ -0,0 +1,5 @@
+/*
+Let a, b be relatively-prime integers. There are
+exactly (1/2)(a-1)(b-1) numbers not of form ax + by
+(x, y >= 0), and the largest is (a-1)(b-1)-1 = ab-a-b.
+*/
---
+++ grundy.cpp
@@ -0,0 +1,16 @@
+/*
+• Player chooses a game and makes a move in it
+Grundy number of a position is xor of grundy
+numbers of positions in summed games.
+• Player chooses a non-empty subset of games
+(possibly, all) and makes moves in all of them
+A position is losing iff each game is in a losing
+position.
+• Player chooses a proper subset of games (not
+empty and not all), and makes moves in all chosen
+ones. A position is losing iff grundy numbers
+of all games are equal.
+• Player must move in all games, and loses if
+can’t move in some game A position is losing
+if any of the games is in a losing position.
+*/
